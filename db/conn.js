63038e2f0f1ad9e3b7c6dbaedbe3297250cd3691
pikupik <santuyaja51@gmail.com>
2023-12-08 16:56:18 +0700

    fixing db

--- db/conn.js
+++ db/conn.js
@@ -1,28 +1,16 @@
-//mengimporr module dari package installet (npm install mysql3)
-const mysql = require("mysql2/promise");
+// File: db/conn.js
 
-//membuat connection
-const pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_DATABASE,
-});
+const mysql = require('mysql2/promise');
 
-//menjadikan function agar bisa di exports
 async function connectToDatabase() {
-  try {
-    const connection = await pool.getConnection();
-    console.log('Connected to MySQL database!');
-    return connection;
-  } catch (error) {
-    console.error('Error connecting to MySQL database:', error.message);
-    throw error;
-  }
+  const connection = await mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+  });
+
+  return connection;
 }
 
-//mengexports function
-module.exports = {
-  connectToDatabase, // ini nama function nya yang di export
-};
+module.exports = { connectToDatabase };
--- economy/start.js
+++ economy/start.js
@@ -1,45 +1,47 @@
 //memanggil module dari db/conn.js
 const { connectToDatabase } = require("../db/conn");
 
-
 //selalu ingat cara ngoding seperti ini, module.exports
 module.exports = {
-  name: "start",
-  description: "Start the journey command",
-  async execute(message, args) {
-    try {
-      //selalu debbuging apakah benar commands sudah di jalankan?
-      console.log("Command /start executed");
-      
-      //melakukan koneksi db
-      const connection = await connectToDatabase();
-      const [rows] = await connection.query(
-        "SELECT * FROM players WHERE users_id = ?",
-        [message.author.id]
-      );
-      
-      //jika Player belum ada, maka buatkan player
-      if (rows.length === 0) {
-        await connection.query(
-          "INSERT INTO players (users_id, balance, inventory, health, exp, level) VALUES (?, ?, CAST(? AS JSON), ?, ?, ?)",
-          [message.author.id, 1000000, "[]", 100, 0, 1]
-        );
-        // pesan yg akan muncul di discord
-        await message.reply(
-          `Bank account created for ${message.author.username}. You have received a gift from buddum of 100000 QC (Quantum Cash) in your Bank Account. Next, buy some sword for hunting!`
-        );
-      } else {
-        //Jika Sudah melakukan /start
-        await message.reply(
-          `You already have a journey, ${message.author.username}.`
-        );
-      }
+    name: "start",
+    description: "Start the journey command",
+    async execute(message, args) {
+        try {
+            //selalu debbuging apakah benar commands sudah di jalankan?
+            console.log("Command /start executed");
 
-      // Pastikan untuk menutup koneksi setelah digunakan
-      connection.end();
-      //menangkap error
-    } catch (error) {
-      console.error("Error in start command:", error);
+            //melakukan koneksi db
+            const connection = await connectToDatabase();
+            console.log("Before query execution");
+            const [rows, fields] = await connection.query(
+                "SELECT * FROM players WHERE users_id = ?",
+                [message.author.id]
+            );
+            console.log("After query execution");
+
+            //jika Player belum ada, maka buatkan player
+            if (rows.length === 0) {
+                await connection.query(
+                    "INSERT INTO players (users_id, balance, inventory, health, exp, level) VALUES (?, ?, ?, ?, ?, ?)",
+                    [message.author.id, 1000000, "[]", 100, 0, 1]
+                );
+
+                // pesan yg akan muncul di discord
+                await message.reply(
+                    `Bank account created for ${message.author.username}. You have received a gift from buddum of 100000 QC (Quantum Cash) in your Bank Account. Next, buy some sword for hunting!`
+                );
+            } else {
+                //Jika Sudah melakukan /start
+                await message.reply(
+                    `You already have a journey, ${message.author.username}.`
+                );
+            }
+
+            // Pastikan untuk menutup koneksi setelah digunakan
+            connection.end();
+            //menangkap error
+        } catch (error) {
+            console.error("Error in start command:", error);
+        }
     }
-  },
 };
